536c1f199a9f221f19ec2675f41fb4e4fc9fdae7
winverse <public.winverse@gmail.com>
2025-09-07 09:14:28 +0900

    feat: initialize user data in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 import styles from './App.module.css';
 
+const initialUsers = [
+  { id: 'u3', name: '박노드', email: 'parknode@example.com' },
+  { id: 'u1', name: '김코딩', email: 'kimcoding@example.com' },
+  { id: 'u2', name: '이해커', email: 'leehacker@example.com' },
+];
+
 function App() {
+  const [users, setUsers] = useState(initialUsers);
   return (
     <div className={styles.appContainer}>
       <h1>유저 목록</h1>
