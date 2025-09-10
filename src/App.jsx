1ef714b98cbed86917659a34cdd17a468e6b5c43
winverse <public.winverse@gmail.com>
2025-09-11 02:46:04 +0900

    API: Pagination 컴포넌트 추가 및 PostList에서 페이지네이션 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { PostProvider } from '@/providers/PostProvider';
 import { Spinner } from './components/Spinner';
+import { Pagination } from './components/Pagination';
 
 function App() {
   return (
     <PostProvider>
       <Spinner />
+      <Pagination currentPage={1} totalPages={10} />
     </PostProvider>
   );
 }
