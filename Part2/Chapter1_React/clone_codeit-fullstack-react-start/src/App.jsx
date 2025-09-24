c6fa566a4cb2426f1000de150e772677582b25a5
powerima <powerima@naver.com>
2025-09-24 17:47:50 +0900

    refactor: App 컴포넌트

--- Part2/Chapter1_React/clone_codeit-fullstack-react-start/src/App.jsx
+++ Part2/Chapter1_React/clone_codeit-fullstack-react-start/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>오늘의 할 일</h1>
+      <section>
+        <input type="text" placeholder="새로운 할 일을 입력하세요" />
+        <button>추가</button>        
+      </section>
+      <section>
+        <ul className="todo-list">
+          <li>리액트 기초 배우기</li>
+          <li>To-Do List 만들어보기</li>
+        </ul>
+      </section>
+    </div>
+  );
 }
 
 export default App
