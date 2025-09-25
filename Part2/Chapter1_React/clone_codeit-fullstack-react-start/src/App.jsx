dce0fdfe93f84b0b1b48c0a27e7a70810e2958c1
powerima <powerima@naver.com>
2025-09-25 14:50:12 +0900

    feat: Adds interactive todo input with state and styling

--- Part2/Chapter1_React/clone_codeit-fullstack-react-start/src/App.jsx
+++ Part2/Chapter1_React/clone_codeit-fullstack-react-start/src/App.jsx
@@ -15,25 +15,44 @@
 import './App.css';
 import { Panel } from './components/Panel';
 import { TodoItem } from './components/TodoItem';
+import { useState } from 'react';
 
 function App() {
-  const title = '오늘의 할 일';
-  const todos = ['리액트 기초 배우기', 'To-Do List 만들어보기'];
+  
+  const [todos, setTodos] = useState(['리액트 기초 배우기', 'To-Do List 만들어보기']);
+  const [inputText, setInputText] = useState('');
+  
+    const handleInputChange = (event) => {
+      setInputText(event.target.value);
+    };
 
+  const handleKeyDown = (event) => {
+    // 한글 입력 중일 때는 Enter 키 동작을 무시
+    if(event.nativeEvent.isComposing) return;
+    if(event.key != 'Enter') return;
+    handleAddTodo();
+  };
+  
+  const handleAddTodo = () => {
+    if (inputText.trim() === '') return;
+    setTodos((prev) => [...prev, inputText]);
+    setInputText('');
+  };
+  
   return (
     <div className="app-container">
       <div className="title-container">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">오늘의 할 일</h1>
         <p className="today">오늘은 {new Date().toLocaleDateString()} 입니다.</p>
       </div>
       <Panel>
-        <div className="input-section">
-          <input type="text" placeholder="새로운 할 일을 입력하세요" />
-          <button>추가</button>        
+        <div className="input-container">
+          <input type="text" className="todo-input" placeholder="새로운 할 일을 입력하세요"
+            value={inputText} onChange={handleInputChange} onKeyDown={handleKeyDown} />
+          <button className="add-button" onClick={handleAddTodo}>추가</button>        
         </div>
       </Panel>
       <Panel>
-      <div className="list-section">
         {todos.length === 0 ? (
           <p className="no-todos">할 일이 없습니다!</p>
         ) : (
@@ -43,7 +62,6 @@ function App() {
             ))}
           </ul>
         )}
-      </div>
       </Panel>
     </div>
   );
